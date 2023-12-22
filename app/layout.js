121f25a7fdcf76156019366cb42d58012569fd7e
loozebox <loozebox@Loozebox.Loozebox>
2023-12-22 16:43:22 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -8,12 +8,14 @@ const dosis = Dosis({ subsets: ['latin'] })
 export const metadata = {
   title: 'Vidnaija',
   description: 'Download movies, hollywood, bollywood, korean series, tv series and anime series for free',
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className=' scrollbar-thumb-[#141935] scrollbar-[#03091A] scrollbar-thin select-none'>
       <body className={dosis.className}>
+      <link rel="icon" href="/logo.png" sizes="any" />
         <Navbar/>
         {children}
         <Footer date={"2023"}/>
