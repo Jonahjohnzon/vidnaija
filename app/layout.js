577396e1b23af48fa4688ee14b9b0ec5869e8949
loozebox <loozebox@Loozebox.Loozebox>
2023-12-24 00:37:49 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className=' scrollbar-thumb-[#141935] scrollbar-[#03091A] scrollbar-thin select-none'>
+      <head><meta name="monetag" content="7aeece893954aa98d10a7f34691f1d31"/></head>
       <body className={dosis.className}>
       <link rel="icon" href="/logo.png" sizes="any" />
         <Navbar/>
