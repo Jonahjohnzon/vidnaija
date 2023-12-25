cc3aaa33ea62f7659608fbba8aff8b433c3f6604
loozebox <loozebox@Loozebox.Loozebox>
2023-12-25 08:32:20 +0100

    first commit

--- app/remove.js
+++
@@ -1,6 +0,0 @@
-export const removeScript = () => {
-    const scriptElement = document.querySelector('script[src="https://arvigorothan.com/tag.min.js"]');
-    if (scriptElement) {
-      scriptElement.parentNode.removeChild(scriptElement);
-    }
-  };
