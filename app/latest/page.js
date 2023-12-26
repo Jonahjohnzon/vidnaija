484db85509488607a3b75803c2cef4b0f095549e
loozebox <loozebox@Loozebox.Loozebox>
2023-12-26 12:37:26 +0100

    first commit

--- app/latest/page.js
+++ app/latest/page.js
@@ -56,7 +56,7 @@ const page = () => {
                   const tit = e.title+ "  " + `S${s}E${ep}`
                   return(
                     
-                    <a href={`/showcase/${e._id}`} onClick={()=>window.open("//greewepi.net/4/6809571")} className=' flex flex-col items-center group  mb-5 px-2 ' key={e._id}>
+                    <a href={`/showcase/${e._id}`} onClick={()=>window.open("//greewepi.net/4/6809571")} className=' flex flex-col items-center group  mb-5 px-2 lg:mx-3' key={e._id}>
                       <div><div style={{backgroundImage:`url(${e.image})`}} className=' rounded-full border-white border-[2px] group-hover:border-yellow-500 w-[100px]  flex justify-center items-center h-[100px] bg-cover group sm:cursor-pointer' onClick={()=>router.push()}></div></div>
                       <div  className=' mb-5 w-[100px] text-white  sm:cursor-pointer group-hover:text-yellow-500 text-center break-all' >{tit.toUpperCase()}</div></a>
                   )
