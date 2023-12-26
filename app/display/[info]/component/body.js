dcc230d8bafce936b6dc6c7b989c0493811033c4
loozebox <loozebox@Loozebox.Loozebox>
2023-12-26 22:12:48 +0100

    first commit

--- app/display/[info]/component/body.js
+++ app/display/[info]/component/body.js
@@ -10,7 +10,7 @@ const router = useRouter()
         const comment = e.comment.length
         
         return(
-            <div key={e._id} className=' mb-10' onClick={()=>router.push(`/showcase/${e._id}`)}>
+            <div key={e._id} className=' mb-10 mx-3' onClick={()=>router.push(`/showcase/${e._id}`)}>
                 <div style={{backgroundImage:`url(${e.image})`}} className=' relative w-[200px]  flex justify-center mb-2 items-center h-[300px] bg-cover group sm:cursor-pointer'>
                     <div className=' absolute w-full h-full bg-black z-30 opacity-20 group-hover:opacity-70 transition-all duration-500 '></div>
                     <div className=' relative z-40 opacity-0 group-hover:opacity-100 flex flex-col items-center ease-in-out transition-all duration-700'>
@@ -22,7 +22,7 @@ const router = useRouter()
                     </div>
                 </div>
                 <div>
-                <p className=' font-bold text-white text-sm'>{e.title}</p>
+                <p className=' font-bold text-white text-sm w-[200px] '>{e.title}</p>
                         <div className=' flex items-center'><div className=' text-yellow-400 mr-1'><FaStar /></div><div className=' font-semibold text-white'>{e.rating} <span className=' text-xs font-bold text-[#A5AFBE]'>/10</span></div></div>
                 </div>
             </div>
