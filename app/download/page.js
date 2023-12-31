ab17d3f3ca121eb784d04ead004c71a185e02a45
loozebox <loozebox@Loozebox.Loozebox>
2023-12-31 20:14:50 +0100

    first commit

--- app/download/page.js
+++ app/download/page.js
@@ -21,7 +21,7 @@ console.log(searchParams)
     <div className=' min-h-[100vh] pt-40 pb-20 flex justify-center items-center'>
       <div className=' flex flex-col justify-center items-centers'>
         <div className='flex justify-center mb-10'><img src='/logos.png' className=' sm:w-24 w-12'/></div>
-        <div className=' flex justify-center mb-10 relative'><a className=' bg-red-600 hover:bg-red-800 text-white px-9 py-2 rounded-3xl font-semibold' href={searchParams.uri}  >DOWNLOAD</a></div>
+        <div className=' flex justify-center mb-10 relative'><Link className=' bg-red-600 hover:bg-red-800 text-white px-9 py-2 rounded-3xl font-semibold' href={searchParams.uri}  >DOWNLOAD</Link></div>
       <div className='px-5'><Note/></div>
       </div>
     </div>}
